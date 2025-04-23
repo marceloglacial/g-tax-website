089eddf6a620830993b9b89c0707c2ef7d75bcb5
Marcelo <glacial@gmail.com>
2025-04-23 17:50:19 -0400

    Add linkt to the top

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,7 +6,9 @@ const Footer = () => {
     <section id="footer" className="max-w-7xl mx-auto py-8">
       <Animation>
         <footer className="flex flex-col justify-between gap-4 border-t pt-8 text-sm font-medium text-muted-foreground md:flex-row md:items-center">
-          <Logo size="sm" />
+          <a href="#">
+            <Logo size="sm" />
+          </a>
           <p>
             desenvolvido por{" "}
             <a
